ea7d5b8581989b55425807d95d84f7a296de6ec8
christianhelms <helmsmedia@gmail.com>
2022-02-28 20:42:04 -0600

    Tailwinds CSS should work

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <title>QR Code Practice</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div><p className='test bg-grey size-xl'>Hello, World!</p></div>
+      <div><p className='test bg-gray-500'>Hello, World!</p></div>
     </div>
   )
 }

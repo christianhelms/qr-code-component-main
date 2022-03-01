4c47c3fa586c8d11ce80c98291418ed930e45bfd
christianhelms <helmsmedia@gmail.com>
2022-02-28 20:58:26 -0600

    updated readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,11 @@ const Home = () => {
         <title>QR Code Practice</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div><p className='test bg-gray-500'>Hello, World!</p></div>
+      <div className='place-content-center'>
+        <div>
+          
+        </div>
+      </div>
     </div>
   )
 }

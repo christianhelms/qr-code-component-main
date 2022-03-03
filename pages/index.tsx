e898ebd6351a96bfa81bff2abf18e71e57577d64
christianhelms <helmsmedia@gmail.com>
2022-03-02 21:48:08 -0600

    added styling in (just needs tweaks)

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,15 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid place-items-center h-screen'>
-        <div className='relative w-66 h-96 bg-white rounded-2xl	p-5 pb-10'>
+        <div className='w-64 h-96 bg-white rounded-2xl	p-3 pb-10'>
           <Image
+            className='rounded-2xl'
             src={'/image-qr-code.png'}
-            width={75}
-            height={75}
+            width={300}
+            height={300}
             priority
            />
-          <p className='font-outfit font-bold text-center'>Improve your front-end skills by building projects</p>
+          <p className='font-outfit font-bold text-center pb-2'>Improve your front-end skills by building projects</p>
           <p className='font-outift font-normal text-center text-sm'>Scan the QR code to visit Front-End Mentor and take your coding skills to the next level.</p>
         </div>
       </div>

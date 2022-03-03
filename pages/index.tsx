dfd30eb10dcc328fea7eb4527e9c26d1962ee157
christianhelms <helmsmedia@gmail.com>
2022-03-02 21:24:37 -0600

    added padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,15 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid place-items-center h-screen'>
-        <div className='relative w-66 h-96 bg-white rounded-2xl	'>
+        <div className='relative w-66 h-96 bg-white rounded-2xl	p-5 pb-10'>
           <Image
-            alt={'qr code to frontend mentor'}
             src={'/public/image-qr-code.png'}
             width={150}
             height={300}
             priority
            />
-          <div className="bg-[url('../public/image-qr-code.png')]"></div>
-          <p className='font-outfit font-bold'>Improve your front-end skills by building projects</p>
-          <p className='font-outift font-normal'>Scan the QR code to visit Front-End Mentor and take your coding skills to the next level.</p>
+          <p className='font-outfit font-bold text-center'>Improve your front-end skills by building projects</p>
+          <p className='font-outift font-normal text-center'>Scan the QR code to visit Front-End Mentor and take your coding skills to the next level.</p>
         </div>
       </div>
     </div>
